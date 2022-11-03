f0fd3106fbc37474c42e37aaf56615dffff61507
Tobias Kaatz <tobias.kaatz@googlemail.com>
2022-11-03 21:02:52 +0100

    Commandline runner

--- src/main/java/eu/kaatz/camunda/restclient/CamundaRestApplication.java
+++ src/main/java/eu/kaatz/camunda/restclient/CamundaRestApplication.java
@@ -1,13 +1,26 @@
 package eu.kaatz.camunda.restclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CamundaRestApplication {
+public class CamundaRestApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(CamundaRestApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamundaRestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}", i, args[i]);
+		}
+	}
 }
