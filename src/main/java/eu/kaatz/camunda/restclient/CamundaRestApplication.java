b88ce8ac0ac5dbfc567547daa711380893f3d16e
Tobias Kaatz <tobias.kaatz@googlemail.com>
2022-11-03 18:35:20 +0100

    import start.spring.io

---
+++ src/main/java/eu/kaatz/camunda/restclient/CamundaRestApplication.java
@@ -0,0 +1,13 @@
+package eu.kaatz.camunda.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kaatz/camunda/restclient/CamundaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kaatz.camunda.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
